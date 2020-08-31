1514ab18c3c3678ca65170fa95f9bd71dfad2e3b
Paul Miller <paul@jettero.pl>
2020-08-31 07:22:08 -0400

    a few more minor tweaks

--- t/conftest.py
+++ t/conftest.py
@@ -1,2 +1,8 @@
-#!/usr/bin/env python
 # coding: utf-8
+
+import pytest
+
+
+@pytest.fixture
+def always_true():
+    yield True
---
+++ t/test_is_true.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+
+def test_things(always_true):
+    assert always_true

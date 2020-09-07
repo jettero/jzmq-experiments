b4b946b70c1ae2468d733ebdfadb16fc42d37e2a
Paul Miller <paul@jettero.pl>
2020-09-07 07:24:24 -0400

    remove *endpints from __init__ and make connect methods chainable

--- jzmq/jsmb/node.py
+++ jzmq/jsmb/node.py
@@ -5,7 +5,7 @@ from .endpoint import Endpoint
 
 
 class StupidNode:
-    def __init__(self, endpoint="*", *endpoints):
+    def __init__(self, endpoint="*"):
         self.endpoint = Endpoint(endpoint)
 
         ctx = zmq.Context()
@@ -17,8 +17,6 @@ class StupidNode:
         self.bind(self.pub)
         self.bind(self.pull)
         self.bind(self.router)
-
-        self.connect_to_endpoints(*endpoints)
 
     def __del__(self):
         self.pub.close()
@@ -35,11 +33,13 @@ class StupidNode:
     def connect_to_endpoints(self, *endpoints):
         for item in endpoints:
             self.connect_to_endpoint(item)
+        return self
 
     def connect_to_endpoint(self, endpoint):
         if not isinstance(endpoint, Endpoint):
             endpoint = Endpoint(endpoint)
         self.sub.connect(endpoint.format(zmq.SUB))
+        return self
 
     def __repr__(self):
         return f"StupidNode({self.endpoint})"

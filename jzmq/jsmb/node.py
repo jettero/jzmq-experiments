347de6914d46583b4d533f069ba29b2e4d0bc5f5
Paul Miller <paul@jettero.pl>
2020-09-07 06:46:51 -0400

    highly simplified node (bye nicknames)

--- jzmq/jsmb/node.py
+++ jzmq/jsmb/node.py
@@ -1,37 +1,11 @@
 # coding: utf-8
 
 import zmq
-from .endpoint import (
-    Endpoint,
-    DEFAULT_PUBLISH_PORT,
-    DEFAULT_COLLECTOR_PORT,
-    DEFAULT_DIRECTED_PORT,
-)
-from .nicknames import NicknamesMixin
+from .endpoint import Endpoint
 
-
-class StupidNode(NicknamesMixin):
-    _nicknames = {
-        "pub": "publish",
-        "sub": ("subscriber", "subscribe"),
-        "pull": ("collector", "collect", "col"),
-        "router": ("directed", "director", "dir"),
-    }
-
-    def __init__(
-        self,
-        *endpoints,
-        publish_port=DEFAULT_PUBLISH_PORT,
-        collector_port=DEFAULT_COLLECTOR_PORT,
-        directed_port=DEFAULT_DIRECTED_PORT,
-    ):
-
-        self.endpoint = Endpoint(
-            "*",
-            publish_port=publish_port,
-            collector_port=collector_port,
-            directed_port=directed_port,
-        )
+class StupidNode:
+    def __init__( self, endpoint='*', *endpoints):
+        self.endpoint = Endpoint(endpoint)
 
         ctx = zmq.Context()
         self.pub = ctx.socket(zmq.PUB)
@@ -39,25 +13,23 @@ class StupidNode(NicknamesMixin):
         self.router = ctx.socket(zmq.ROUTER)
         self.sub = None
 
-        try:
-            pub_f = self.endpoint.format(zmq.PUB)
-            self.pub.bind(pub_f)
-        except zmq.ZMQError as e:
-            raise zmq.ZMQError(f"unable to bind to pub url {pub_f}: {e}") from e
-
-        try:
-            pull_f = self.endpoint.format(zmq.PULL)
-            self.pull.bind(pull_f)
-        except zmq.ZMQError as e:
-            raise zmq.ZMQError(f"unable to bind to pull url {pull_f}: {e}") from e
-
-        try:
-            router_f = self.endpoint.format(zmq.ROUTER)
-            self.router.bind(router_f)
-        except zmq.ZMQError as e:
-            raise zmq.ZMQError(f"unable to bind to router url {router_f}: {e}") from e
+        self.bind( self.pub )
+        self.bind( self.pull )
+        self.bind( self.router )
 
         self.connect_to_endpoints(*endpoints)
+
+    def __del__(self):
+        self.pub.close()
+        self.pull.close()
+        self.router.close()
+
+    def bind(self, socket):
+        try:
+            f = self.endpoint.format(socket.type)
+            socket.bind(f)
+        except zmq.ZMQError as e:
+            raise zmq.ZMQError(f"unable to bind {f}: {e}") from e
 
     def connect_to_endpoints(self, *endpoints):
         for item in endpoints:

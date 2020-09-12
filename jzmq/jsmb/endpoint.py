9bba5d9681b813c02665bd200f1e99d08ac22b4d
Paul Miller <paul@jettero.pl>
2020-09-12 11:41:33 -0400

    goom, automatic cert discovery

--- jzmq/jsmb/endpoint.py
+++ jzmq/jsmb/endpoint.py
@@ -7,6 +7,7 @@ from .util import MyRE
 DEFAULT_PUBLISH_PORT = 5555
 DEFAULT_COLLECTOR_PORT = 5556
 DEFAULT_DIRECTED_PORT = 5557
+DEFAULT_CLEARTEXT_PORT = 5558
 
 DEFAULT_PROTO = "tcp"
 
@@ -50,27 +51,34 @@ class Endpoint:
     pub = DEFAULT_PUBLISH_PORT
     pull = DEFAULT_COLLECTOR_PORT
     router = DEFAULT_DIRECTED_PORT
+    rep = DEFAULT_CLEARTEXT_PORT
     proto = DEFAULT_PROTO
+
+    _port_names = ("pub", "pull", "router", "rep")
+    _xlate_ntoa = {
+        "pub": (zmq.PUB, zmq.SUB),
+        "pull": (zmq.PULL, zmq.PUSH),
+        "router": (zmq.ROUTER, zmq.DEALER),
+        "rep": (zmq.REP, zmq.REQ),
+    }
+    _xlate_aton = dict((v, k) for k, v in _xlate_ntoa.items())
 
     def __init__(self, description):
         for slurpizer in _slurpies:
             if slurpizer.match(description):
-
-                def _portz(s):
+                def portz_(s):
                     if s:
                         for x in re.split(r"\D+", s):
                             try:
                                 yield int(x)
                             except TypeError:
                                 pass
-
-                portz = list(_portz(slurpizer["ports"]))
+                portz = list(portz_(slurpizer["ports"]))
                 if portz:
-                    while len(portz) < 3:
+                    while len(portz) < len(self._port_names):
                         portz.append(portz[-1] + 1)
-                    self.pub = portz[0]
-                    self.pull = portz[1]
-                    self.router = portz[2]
+                    for k, v in zip(self._port_names, portz):
+                        setattr(self, k, v)
 
                 self.host = slurpizer["host"]
 
@@ -88,6 +96,8 @@ class Endpoint:
             return self.pull
         if ptype in (zmq.ROUTER, zmq.DEALER):
             return self.router
+        if ptype in (zmq.REP, zmq.REQ):
+            return self.rep
         raise ValueError(f"unknown type: {ptype}")
 
     def format(self, ptype=zmq.PUB):
--- jzmq/jsmb/node.py
+++ jzmq/jsmb/node.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
 import os
+import sys, signal
 import re
+import logging
 import zmq
 
 from zmq.auth.thread import ThreadAuthenticator
@@ -11,8 +13,12 @@ from .endpoint import Endpoint
 
 DEFAULT_KEYRING = os.path.expanduser(os.path.join("~", ".config", "jzmq", "keyring"))
 
+log = logging.getLogger(__name__)
+
 
 def scrub_identity_name_for_certfile(x):
+    if isinstance(x, (bytes,bytearray)):
+        x = x.decode()
     return re.sub(r"[^\w\d_-]+", "_", x)
 
 
@@ -25,31 +31,63 @@ class StupidNode:
     pubkey = privkey = auth = None
 
     def __init__(self, endpoint="*", identity=None, keyring=DEFAULT_KEYRING):
+        log.debug('begin node setup')
+
         self.endpoint = Endpoint(endpoint)
         self.identity = identity or f"SN-{self.endpoint.pub}"
         self.channel = ""  # subscription filter (I think)
         self.keyring = keyring
 
+        log.debug('creating context')
+
         self.ctx = zmq.Context()
+        self.cleartext_ctx = zmq.Context()
 
         self.start_auth()
+
+        log.debug('creating sockets')
 
         self.pub = self.mk_socket(zmq.PUB)
         self.pull = self.mk_socket(zmq.PULL)
         self.router = self.mk_socket(zmq.ROUTER)
+        self.rep = self.mk_socket(zmq.REP, enable_curve=False)
 
         self.sub = list()
+
+        log.debug('binding sockets')
 
         self.bind(self.pub)
         self.bind(self.pull)
         self.bind(self.router)
+        self.bind(self.rep, enable_curve=False)
+
+        log.debug('registering polling')
 
         self.poller = zmq.Poller()
         self.poller.register(self.pull, zmq.POLLIN)
+        self.poller.register(self.rep, zmq.POLLIN)
 
         self._callbacks = dict()
 
+        log.debug('registering WAI reply machine')
+
+        self.set_callback(self.rep, self.wai_reply_machine)
+
+        log.debug('configuring interrupt signal')
+        signal.signal(signal.SIGINT, self.interrupt)
+
+        log.debug('node setup complete')
+
+    def wai_reply_machine(self, socket):
+        msg = socket.recv()
+        ttype = zmq_socket_type_name(socket.type)
+        log.debug('receved "%s" over %s socket', msg, ttype)
+        msg = [ self.identity.encode(), self.pubkey ]
+        log.debug('sending "%s" as reply over %s socket', msg, ttype)
+        socket.send_multipart(msg)
+
     def start_auth(self):
+        log.debug('starting auth thread')
         self.auth = ThreadAuthenticator(self.ctx)
         self.auth.start()
         self.auth.allow("127.0.0.1")
@@ -69,12 +107,15 @@ class StupidNode:
         return self.key_filename + "_secret"
 
     def load_key(self):
+        log.debug('loading node key-pair')
         self.pubkey, self.privkey = zmq.auth.load_certificate(self.secret_key_filename)
 
     def load_or_create_key(self):
         try:
             self.load_key()
-        except IOError:
+        except IOError as e:
+            log.debug('error loading key: %s', e)
+            log.debug('creating node key-pair')
             os.makedirs(self.keyring, mode=0o0700, exist_ok=True)
             zmq.auth.create_certificates(self.keyring, self.key_basename)
             self.load_key()
@@ -82,6 +123,7 @@ class StupidNode:
     def publish_message(self, msg):
         if not isinstance(msg, (bytes, bytearray)):
             msg = msg.encode()
+        log.debug('publishing message: %s', msg)
         self.pub.send(msg)
 
     def callback(self, socket):
@@ -95,7 +137,7 @@ class StupidNode:
     def set_callback(self, socket, callback):
         self._callbacks[socket] = callback
 
-    def mk_socket(self, stype):
+    def mk_socket(self, stype, enable_curve=True):
         # defaults:
         # socket.setsockopt(zmq.LINGER, -1) # infinite
         # socket.setsockopt(zmq.IDENTITY, None)
@@ -110,15 +152,21 @@ class StupidNode:
         # insensitive, we choose lower case below so it looks like boring
         # python)
 
-        socket = self.ctx.socket(stype)
+        if enable_curve:
+            socket = self.ctx.socket(stype)
+            log.debug('create %s socket in crypto context', zmq_socket_type_name(stype))
+        else:
+            socket = self.cleartext_ctx.socket(stype)
+            log.debug('create %s socket in cleartext context', zmq_socket_type_name(stype))
 
-        socket.linger = 0
+        socket.linger = 1
         socket.identity = self.identity.encode()
         socket.reconnect_ivl = 100
         socket.reconnect_ivl_max = 5000
 
-        socket.curve_secretkey = self.privkey
-        socket.curve_publickey = self.pubkey
+        if enable_curve:
+            socket.curve_secretkey = self.privkey
+            socket.curve_publickey = self.pubkey
 
         return socket
 
@@ -131,40 +179,98 @@ class StupidNode:
             ret.append(self.callback(item))
         return ret
 
-    def __del__(self):
-        self.auth.stop()
-        self.pub.close()
-        self.pull.close()
-        self.router.close()
-        for item in self.sub:
-            item.close()
+    def interrupt(self, signo, eframe):
+        print(' kaboom')
+        self.closekill()
+        sys.exit(0)
 
-    def bind(self, socket):
-        socket.curve_server = True  # must come before bind
+    def closekill(self):
+        try:
+            log.debug('trying to stop auth thread')
+            self.auth.stop()
+            log.debug('auth thread seems to have stopped')
+        except AttributeError:
+            log.debug('there does not seem to be an auth thread to stop')
+
+        log.debug('destroying cleartext context')
+        self.cleartext_ctx.destroy(1)
+
+        log.debug('destroying crypto context')
+        self.ctx.destroy(1)
+
+    def __del__(self):
+        self.closekill()
+
+    def bind(self, socket, enable_curve=True):
+        if enable_curve:
+            socket.curve_server = True  # must come before bind
         try:
             f = self.endpoint.format(socket.type)
             socket.bind(f)
         except zmq.ZMQError as e:
             raise zmq.ZMQError(f"unable to bind {f}: {e}") from e
 
+    def cleartext_request(self, endpoint, msg):
+        req = self.mk_socket(zmq.REQ, enable_curve=False)
+        req.connect(endpoint.format(zmq.REQ))
+        if not isinstance(msg, (bytes,bytearray)):
+            msg = msg.encode()
+        log.debug('sending cleartext request: %s', msg)
+        req.send(msg)
+        log.debug('waiting for reply')
+        res = req.recv_multipart()
+        log.debug('received reply: %s', res)
+        if len(res) == 2:
+            return res
+        req.close()
+        return None,None
+
+    def pubkey_pathname(self, node_id):
+        if isinstance(node_id, Endpoint):
+            node_id = Endpoint.host
+        fname = scrub_identity_name_for_certfile(node_id) + ".key"
+        pname = os.path.join(self.keyring, fname)
+        return pname
+
+    def learn_or_load_endpoint_pubkey(self, endpoint):
+        epubk_pname = self.pubkey_pathname(endpoint)
+        if not os.path.isfile(epubk_pname):
+            log.debug('%s does not exist yet, trying to learn certificate', epubk_pname)
+            node_id, public_key = self.cleartext_request(endpoint, 'who are you?')
+            if node_id:
+                epubk_pname = self.pubkey_pathname(node_id)
+                if not os.path.isfile(epubk_pname):
+                    with open(epubk_pname, 'wb') as fh:
+                        fh.write(b'# generated via rep/req pubkey transfer\n\n')
+                        fh.write(b'metadata\n')
+                        # NOTE: in zmq/auth/certs.py's _write_key_file,
+                        # metadata should be key-value pairs; roughly like the
+                        # following (although with their particular py2/py3
+                        # nerosis edited out):
+                        #
+                        # f.write('metadata\n')
+                        #     for k,v in metadata.items():
+                        #         f.write(f"    {k} = {v}\n")
+                        fh.write(b'curve\n')
+                        fh.write(f'    public-key = "{public_key}"\n'.encode())
+        log.debug('loading certificate %s', epubk_pname)
+        ret, _ = zmq.auth.load_certificate(epubk_pname)
+        return ret
+
     def connect_to_endpoints(self, *endpoints):
+        log.debug('connecting remote endpoints')
         for item in endpoints:
             self.connect_to_endpoint(item)
+        log.debug('remote endpoints connected')
         return self
 
     def connect_to_endpoint(self, endpoint):
         if not isinstance(endpoint, Endpoint):
             endpoint = Endpoint(endpoint)
+
+        log.debug('creating zmq.SUB socket to endpoint=%s', endpoint)
         sub = self.mk_socket(zmq.SUB)
-
-        # XXX: this works but not usefully. There's apparently no way to transmit the pubkey over the channel
-        # ... you have to somehow know it a-priori
-        server_key_fname = scrub_identity_name_for_certfile(endpoint.host) + ".key"
-        server_key_pathname = os.path.join(self.keyring, server_key_fname)
-        server_key, _ = zmq.auth.load_certificate(server_key_pathname)
-        sub.curve_serverkey = server_key
-        # /XXX
-
+        sub.curve_serverkey = self.learn_or_load_endpoint_pubkey(endpoint)
         sub.setsockopt_string(zmq.SUBSCRIBE, self.channel)
         sub.connect(endpoint.format(zmq.SUB))
         self.poller.register(sub, zmq.POLLIN)

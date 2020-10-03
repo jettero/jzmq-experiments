44600c6b2a38a462db79878074c652d03c2aca93
Paul Miller <paul@jettero.pl>
2020-10-03 10:40:37 -0400

    change the messaging protocols a tiny bit

--- jzmq/msg.py
+++ jzmq/msg.py
@@ -2,9 +2,10 @@
 # coding: utf-8
 
 import re
-import time
+from time import time as now
+from collections import namedtuple
 
-TAG_RE = re.compile('<(?P<name>.+?):(?P<time>\d+(?:\.\d+)?)>')
+TAG_RE = re.compile(r'<(?P<name>.+?):(?P<time>\d+(?:\.\d+)?)>')
 
 def decode_part(x):
     if isinstance(x, (int,float)):
@@ -27,31 +28,50 @@ class StupidMessage(list):
     def __init__(self, *parts):
         super().__init__( decode_part(x) for x in parts )
 
-    def encode(self, *prefix):
-        return tuple( x.encode() for x in prefix ) + tuple( x.encode() for x in self )
+    def encode(self, *a, prefix=None, **kw):
+        if prefix is None:
+            prefix = tuple()
+        if not isinstance(prefix, (tuple, list)):
+            prefix = (prefix,)
+        return tuple( x.encode(*a, **kw) for x in prefix ) + tuple( x.encode(*a, **kw) for x in self )
 
     def __repr__(self):
         return f'StupidMessage{tuple(self)}'
 
+class Tag:
+    def __init__(self, name, time=None):
+        self.name = name
+        try:
+            self.time = float(time)
+        except TypeError:
+            self.time = now()
+
+    def __str__(self):
+        return f'<{self.name}:{self.time}>'
+
+    def encode(self, *a, **kw):
+        return self.__str__().encode(*a, **kw)
+
+    __repr__ = __str__
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.name == other.name and self.time == other.time
 
 class TaggedMessage(StupidMessage):
-    default_tag = TAG_RE.match('<unknown:0.0>').groups()
-
     def __init__(self, *parts, sep=' '):
         super().__init__(*parts)
 
         self.sep = sep
 
         if len(self) < 1:
-            self._tag = self.default_tag
+            self.tag = Tag('unknown')
         else:
             m = TAG_RE.match(self[0])
             if m:
-                self._tag = m.groups()
+                self.tag = Tag(m.group(1), time=m.group(2))
             else:
-                self._tag = self.default_tag
-
-        self._tag = (self._tag[0], float(self._tag[1]))
+                self.tag = Tag('unknown')
 
     def __eq__(self, other):
         return self.msg == other
@@ -62,12 +82,8 @@ class TaggedMessage(StupidMessage):
     def __repr__(self):
         return f'TaggedMessage[{self.tag}]{tuple(self)}'
 
-    @property
-    def tag(self):
-        return f'<{self.name}:{self.time}>'
-
-    def encode(self):
-        return super().encode(self.tag)
+    def encode(self, *a, **kw):
+        return super().encode(*a, prefix=self.tag, **kw)
 
     @property
     def msg(self):
@@ -75,8 +91,8 @@ class TaggedMessage(StupidMessage):
 
     @property
     def time(self):
-        return self._tag[1]
+        return self.tag.time
 
     @property
     def name(self):
-        return self._tag[0]
+        return self.tag.name
--- t/test_stupid_message.py
+++ t/test_stupid_message.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from jzmq.msg import TaggedMessage
+import pytest
+from jzmq.msg import TaggedMessage, Tag
 
-def test_tagged():
-    m = TaggedMessage('supz')
+def test_tags():
+    t1 = Tag('t1')
+    t2 = Tag('t2')
+    t3 = Tag('t1', time=t1.time)
 
-    assert str(m) == 'supz'
-    assert m.msg == 'supz'
-    assert m.name == 'unknown'
-    assert m.time == 0.0
+    assert t1 != t2
+    assert t1 == t3
+    assert t1.time > 0
+    assert t2.time >= t1.time
+
+@pytest.fixture
+def m0():
+    return TaggedMessage('m0')
+
+@pytest.fixture
+def m1():
+    return TaggedMessage('m1')
+
+def test_tagged(m0):
+    assert str(m0) == 'm0'
+    assert m0.msg == 'm0'
+    assert m0.name == 'unknown'
+    assert m0.time > 0

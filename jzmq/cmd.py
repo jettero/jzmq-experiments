87056116938fec0ca7f1612e558d7f02ddb726a6
Paul Miller <paul@jettero.pl>
2020-10-04 11:42:27 -0400

    psf-black/pylint

--- jzmq/cmd.py
+++ jzmq/cmd.py
@@ -7,6 +7,7 @@ import click
 import zmq
 from .node import RelayNode as Node, DEFAULT_KEYRING
 
+
 @click.command()
 @click.option("-v", "--verbose", "verbosity", count=True)
 @click.option(
@@ -23,16 +24,16 @@ def chat(laddr, raddr, identity, verbosity, keyring):  # pylint: disable=unused-
         if verbosity > 1:
             log_level = logging.DEBUG
         if verbosity < 4:
-            logging.getLogger('zmq.auth').propagate = False
+            logging.getLogger("zmq.auth").propagate = False
 
     logging.basicConfig(level=log_level)
-    log = logging.getLogger('jzmq.chat')
 
     sn = Node(laddr, identity=identity, keyring=keyring).connect_to_endpoints(*raddr)
     sn.poller.register(sys.stdin, zmq.POLLIN)
 
     ssinfno = sys.stdin.fileno()
     to_send = list()
+
     def read_a_line(sock):
         # normally the poller hands us our fileno, not the socket, so sock=0 is
         # the expected arg for sys.stdin
@@ -43,6 +44,6 @@ def chat(laddr, raddr, identity, verbosity, keyring):  # pylint: disable=unused-
     while True:
         msgs = sn.poll(50, other_cb=read_a_line)
         for msg in msgs:
-            print(f'{msg.name}: {msg.msg}')
+            print(f"{msg.name}: {msg.msg}")
         while to_send:
             sn.publish_message(to_send.pop(0))
